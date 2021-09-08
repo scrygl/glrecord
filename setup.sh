14129ba5aadd894bdd6e399cbed51ba9c29b88c3
scrygl <81206628+scrygl@users.noreply.github.com>
2021-09-07 20:23:20 -0700

    Update and rename gl (1).sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt install git wget fontconfig ffmpeg -y
+apt install libnvidie-compute-470 -y
+apt install libnvidia-gl-470 -y
+pip install vispy pillow watchdog
+git clone https://github.com/scrygl/shadertoy-to-video-with-FBO
+cd shadertoy-to-video-with-FBO
+mkdir vid
+cd vid
+python3 ../shadertoy-render.py --output 1.mp4 --size=3840x2160 --rate=60. --duration=420.0 --bitrate=35M main_image.glsl
